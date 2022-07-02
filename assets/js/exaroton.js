54be8fbc238ce264aede86aed7b038a8661645f6
AAWA-byt <mail@aaronwagner.earth>
2022-07-02 22:14:07 +0200

    :zap: moved to init function

--- assets/js/exaroton.js
+++ assets/js/exaroton.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const request = require('request');
 const {Client} = require('exaroton');
-let token = document.querySelector("#token").value;
 
-
-const client = new Client(token);
 
 function init() {
     document.getElementById('button').addEventListener('click', getData);
+    let token = document.querySelector("#token").value;
+    const client = new Client(token);
 }
 
 
